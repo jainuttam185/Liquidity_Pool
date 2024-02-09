0880a8612ae42c406b354e75be12ffb98b20a7ee
jainuttam185 <jainuttam185@gmail.com>
2024-02-09 10:48:22 +0530

    Fourth Commit

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const { Web3 } = require("web3");
+const Web3  = require("web3");
 const compiled = require("./build/Pool.json");
  
 const provider = new HDWalletProvider(
@@ -12,7 +12,7 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   try{
   const accounts = await web3.eth.getAccounts();
-  const constructorArguments = ["0x4e4838Ab7605C24f93AfEa327b0cb261543314f4", "0x21408DbA4e1974AC1dDB426630fe94bf6c4c66c7"];
+  const constructorArguments = ["0x9a5490c9d4e23f7889f0436f1f194c47E45836Fb", "0x8CCfAc6cDe3900033ECb721d94ee71596533cDC7"];
  
   console.log("Attempting to deploy from account", accounts[0]);
  //console.log(compiled.Pool);
--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import Pool from "./build/Pool.json";
  
 const instance = new web3.eth.Contract(
   Pool.abi,
-  "0x3ec0112255f7e439603923a513208a891829c72d"
+  "0x6EfDB9f4B7A48fB0017DBB6596acDa4B60005e29"
 );
  
 export default instance;
